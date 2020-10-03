45ec532052f16b05e30ba780520076e571e74a79
Nishant Ingle <30694286+Nishant-Ingle@users.noreply.github.com>
2020-10-03 14:28:15 +0530

    Improve comparison with None
    * Operators '!=' and '==' call the ' __eq__' method of a class which can give unwanted results.
    * Better way is to use is and is not instead of them.

--- Binary_Search_Tree/main.py
+++ Binary_Search_Tree/main.py
@@ -10,20 +10,20 @@ class binary_search_tree:
 		self.root=None
 
 	def insert(self,value):
-		if self.root==None:
+		if self.root is None:
 			self.root=node(value)
 		else:
 			self._insert(value,self.root)
 
 	def _insert(self,value,cur_node):
 		if value<cur_node.value:
-			if cur_node.left_child==None:
+			if cur_node.left_child is None:
 				cur_node.left_child=node(value)
 				cur_node.left_child.parent=cur_node # set parent
 			else:
 				self._insert(value,cur_node.left_child)
 		elif value>cur_node.value:
-			if cur_node.right_child==None:
+			if cur_node.right_child is None:
 				cur_node.right_child=node(value)
 				cur_node.right_child.parent=cur_node # set parent
 			else:
@@ -32,29 +32,29 @@ class binary_search_tree:
 			print("Value already in tree!")
 
 	def print_tree(self):
-		if self.root!=None:
+		if self.root is not None:
 			self._print_tree(self.root)
 
 	def _print_tree(self,cur_node):
-		if cur_node!=None:
+		if cur_node is not None:
 			self._print_tree(cur_node.left_child)
 			print (str(cur_node.value))
 			self._print_tree(cur_node.right_child)
 
 	def height(self):
-		if self.root!=None:
+		if self.root is not None:
 			return self._height(self.root,0)
 		else:
 			return 0
 
 	def _height(self,cur_node,cur_height):
-		if cur_node==None: return cur_height
+		if cur_node is None: return cur_height
 		left_height=self._height(cur_node.left_child,cur_height+1)
 		right_height=self._height(cur_node.right_child,cur_height+1)
 		return max(left_height,right_height)
 
 	def find(self,value):
-		if self.root!=None:
+		if self.root is not None:
 			return self._find(value,self.root)
 		else:
 			return None
@@ -62,9 +62,9 @@ class binary_search_tree:
 	def _find(self,value,cur_node):
 		if value==cur_node.value:
 			return cur_node
-		elif value<cur_node.value and cur_node.left_child!=None:
+		elif value<cur_node.value and cur_node.left_child is not None:
 			return self._find(value,cur_node.left_child)
-		elif value>cur_node.value and cur_node.right_child!=None:
+		elif value>cur_node.value and cur_node.right_child is not None:
 			return self._find(value,cur_node.right_child)
 
 	def delete_value(self,value):
@@ -76,7 +76,7 @@ class binary_search_tree:
 		# Improvements since prior lesson
 
 		# Protect against deleting a node not found in the tree
-		if node==None or self.find(node.value)==None:
+		if node==None or self.find(node.value) is None:
 			print("Node to be deleted not found in the tree!")
 			return None 
 		## -----
@@ -84,15 +84,15 @@ class binary_search_tree:
 		# returns the node with min value in tree rooted at input node
 		def min_value_node(n):
 			current=n
-			while current.left_child!=None:
+			while current.left_child is not None:
 				current=current.left_child
 			return current
 
 		# returns the number of children for the specified node
 		def num_children(n):
 			num_children=0
-			if n.left_child!=None: num_children+=1
-			if n.right_child!=None: num_children+=1
+			if n.left_child is not None: num_children+=1
+			if n.right_child is not None: num_children+=1
 			return num_children
 
 		# get the parent of the node to be deleted
@@ -109,7 +109,7 @@ class binary_search_tree:
 
 			# Added this if statement post-video, previously if you 
 			# deleted the root node it would delete entire tree.
-			if node_parent!=None:
+			if node_parent is not None:
 				# remove reference to the node from the parent
 				if node_parent.left_child==node:
 					node_parent.left_child=None
@@ -122,14 +122,14 @@ class binary_search_tree:
 		if node_children==1:
 
 			# get the single child node
-			if node.left_child!=None:
+			if node.left_child is not None:
 				child=node.left_child
 			else:
 				child=node.right_child
 
 			# Added this if statement post-video, previously if you 
 			# deleted the root node it would delete entire tree.
-			if node_parent!=None:
+			if node_parent is not None:
 				# replace the node to be deleted with its child
 				if node_parent.left_child==node:
 					node_parent.left_child=child
@@ -156,7 +156,7 @@ class binary_search_tree:
 			self.delete_node(successor)
 
 	def search(self,value):
-		if self.root!=None:
+		if self.root is not None:
 			return self._search(value,self.root)
 		else:
 			return False
@@ -164,8 +164,8 @@ class binary_search_tree:
 	def _search(self,value,cur_node):
 		if value==cur_node.value:
 			return True
-		elif value<cur_node.value and cur_node.left_child!=None:
+		elif value<cur_node.value and cur_node.left_child is not None:
 			return self._search(value,cur_node.left_child)
-		elif value>cur_node.value and cur_node.right_child!=None:
+		elif value>cur_node.value and cur_node.right_child is not None:
 			return self._search(value,cur_node.right_child)
 		return False 
